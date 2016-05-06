6a2b8f4b95641cb4b8bde57b8d3acbe4676d357e
Denis <gotsman-d@mail.ru>
2016-05-06 15:15:11 +0300

    version 0.9 beta

--- src/ua/gotsman/sms/Sms.java
+++ src/ua/gotsman/sms/Sms.java
@@ -1,9 +1,7 @@
 package ua.gotsman.sms;
 
 import jxl.read.biff.BiffException;
-import ua.smsc.sys.soap.Send;
-import ua.smsc.sys.soap.Service;
-import ua.smsc.sys.soap.ServiceSoap;
+import ua.smsc.sys.soap.*;
 
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -14,27 +12,28 @@ import java.nio.file.Paths;
 
 /**
  * Created by Денис on 04/16/2016.
- * Формирует СМС и отправляет его на сервис TurboSMS.ua
+ * Формирует СМС и отправляет его на шлюз smsc.ua через SOAP протокол
  */
 public class Sms {
+    private final static String login = "Severik";
+    private final static String password = "Derparol12!@";
+    private final static String sender = "SoftTechno";
+    private final static String time = "0";
 
     public static void main(String[] args) throws IOException, BiffException {
         Service service = new Service();
         ServiceSoap port = service.getServiceSoap();
         Send send = new Send();
-        send.setLogin("Severik");
-        send.setPsw("Derparol12!@");
-        send.setSender("SoftTechno");
-        send.setTime("0");
         Path path = Paths.get("D:\\1.xls");
-        /*while (true) {
+        while (true) {
             if (Files.exists(path)) {
                 InputStream inputStream = new FileInputStream(String.valueOf(path));
                 Info info = new Info();
                 info.loadFromXls(inputStream);
+
                 inputStream.close();
                 Files.delete(path);
             }
-        }*/
+        }
     }
 }
