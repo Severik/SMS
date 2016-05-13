b6373c5b78fa2074ac8dd98473fcc2c05d9fcacc
Denis <gotsman-d@mail.ru>
2016-05-13 07:44:07 +0300

    Merge branch 'master' of https://github.com/Severik/SMS into github/master
    # Conflicts:
    #	.idea/compiler.xml
    #	.idea/misc.xml
    #	.idea/modules.xml

--- src/ua/gotsman/sms/Controller.java
+++ src/ua/gotsman/sms/Controller.java
@@ -1,9 +1,20 @@
 package ua.gotsman.sms;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 /**
  * Created by denis on 12.05.16.
  *
  */
 public class Controller {
 
+    @FXML
+    public void btnStart(ActionEvent actionEvent) {
+        System.out.println("Поехали!!!");
+    }
+    @FXML
+    public void btnStop(ActionEvent actionEvent) {
+        System.out.println("Стоп машина!!!");
+    }
 }
