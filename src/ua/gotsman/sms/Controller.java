6209f88d1d0b3b25bea4cfa55b1deab74196c30e
Denis <gotsman-d@mail.ru>
2016-06-05 17:54:10 +0300

    добавлена проверка баланса

--- src/ua/gotsman/sms/Controller.java
+++ src/ua/gotsman/sms/Controller.java
@@ -1,17 +1,28 @@
 package ua.gotsman.sms;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
 public class Controller {
 
+    public Button start;
+    public Button stop;
+    public Button balance;
+    private Sms sms = new Sms();
+
     @FXML
     public void btnStart() {
-        Thread thread = new Thread(new Sms());
+        Thread thread = new Thread(sms);
         thread.start();
     }
 
     @FXML
     public void btnStop() {
-        Sms.stopTime = 1;
+        sms.stopTime = 1;
+    }
+
+    @FXML
+    public void btnBalance() {
+        System.out.println(sms.getUserBalance());
     }
 }
