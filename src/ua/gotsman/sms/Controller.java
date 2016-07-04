abcc9b2a18e3c3c9dc8ff83c8cb26490d3284007
Денис <gotsman.denis@gmail.com>
2016-07-04 14:11:50 +0300

    version 1.0 release

--- src/ua/gotsman/sms/Controller.java
+++ src/ua/gotsman/sms/Controller.java
@@ -6,7 +6,6 @@ import javafx.scene.control.*;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.Background;
 import ua.smsc.sys.soap.Send;
 
 import java.awt.*;
